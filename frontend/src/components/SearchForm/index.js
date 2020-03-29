ad548249504fe8975563b4a9bf72970d47f13d99
Keisha <kshep425@gmail.com>
2020-03-29 12:44:04 -0400

    Update index.js

--- frontend/src/components/SearchForm/index.js
+++ frontend/src/components/SearchForm/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
-
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import FormControl from "react-bootstrap/FormControl"
 const SearchForm = () => {
   return (
     <Container>
       <Card border="dark" className="my-3">
+        <Card.Header>
+          Search
+        </Card.Header>
+        <Card.Body>
+          <Form>
+            <Form.Group>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button className="justify-content-end" variant="outline-dark">Search</Button>
+            </Form.Group>
+          </Form>
+        </Card.Body>
       </Card>
     </Container>
   );

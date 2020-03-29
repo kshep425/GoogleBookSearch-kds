3157d4a9a8959c3f956a2c855b03a0a9123f2fdd
Keisha <kshep425@gmail.com>
2020-03-29 11:12:10 -0400

    Added Server.js updated package.json to start frontend and backend concurrently

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+const PORT = process.env.PORT;
+console.log(PORT)
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("./frontend/public"));
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
